fa3cb82ddee0f80f11bb90f00c5686042534c797
MuathZaher <81557758+MuathZahir@users.noreply.github.com>
2024-10-29 10:37:51 +0400

    Update index.tsx

--- src/pages/Content/index.tsx
+++ src/pages/Content/index.tsx
@@ -10,4 +10,4 @@ import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('app-container');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<Popup />);
+root.render(<App />);
