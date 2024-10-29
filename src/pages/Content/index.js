560a27101a4bbaff407d029bd9658c9abb8e7572
MuathZaher <81557758+MuathZahir@users.noreply.github.com>
2024-10-29 10:36:42 +0400

    Update index.js

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -4,3 +4,10 @@ console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app-container');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<Popup />);
